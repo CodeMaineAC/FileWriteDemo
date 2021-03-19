86ce0c485acb21f73a75765ed53adcf6479b5338
Benjamin Goff <bgoff@mainerobotics.org>
2021-03-19 09:18:52 -0400

    Add project files.

---
+++ FileWriteDemo/WriteSomeText.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FileWriteDemo
+{
+    class WriteSomeText
+    {
+        static void Main(string[] args)
+        {
+            FileStream outFile = new 
+                FileStream("SomeText.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(outFile);
+
+            Console.WriteLine("Text to write to file>> ");
+            string text = Console.ReadLine();
+
+            try
+            {
+                writer.WriteLine(text);
+            }
+            finally
+            {
+                writer.Close();
+                outFile.Close();
+            }
+        }
+    }
+}
